71ec02664d30e9f5cad526ba588e61238b4fee46
Heliosaun <119249951+Heliosaun@users.noreply.github.com>
2024-01-09 14:29:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def main():
 @st.cache_data
 def load_data_and_model():
     data = pd.read_csv('data.csv', index_col='SK_ID_CURR', encoding='utf-8')
-    sample = pd.read_csv('data/sample.csv', index_col='SK_ID_CURR', encoding='utf-8')
+    sample = pd.read_csv('sample.csv', index_col='SK_ID_CURR', encoding='utf-8')
     clf = pickle.load(open('model_opti_lgbm_final.pkl', 'rb'))
     return data, sample, clf
 
