fc75c03c77225a2469af74eff53d3fe245669904
Heliosaun <119249951+Heliosaun@users.noreply.github.com>
2024-01-13 11:15:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ class DataResponseModel(BaseModel):
 
 class ShapValuesResponseModel(BaseModel):
     shap_val: List[float]
+    
+class AgeDistributionResponseModel(BaseModel):
+    age_distribution: dict
+
+class IncomeDistributionResponseModel(BaseModel):
+    income_distribution: dict
 
 # Chargement et prétraitement des données
 data_test = pd.read_csv("sample_final_dataset.csv")
@@ -84,6 +90,35 @@ def shap_values_client(id_client: int):
         return {"shap_val": shap_data_flat}
     else:
         raise HTTPException(status_code=404, detail="ID inconnu")
+        
+def calculate_age_in_years_and_months(days):
+    years = -days // 365
+    months = (-days % 365) // 30  # Approximation du nombre de mois
+    return f"{years} ans, {months} mois"
+
+# test local : http://127.0.0.1:8000/credit/430080/age
+@app.get("/credit/{id_client}/age", response_model=AgeDistributionResponseModel)
+def get_age_distribution(id_client: int):
+    if id_client in liste_id:
+        client_index = liste_id.index(id_client)
+        client_days_birth = data_test.iloc[client_index]['DAYS_BIRTH']
+        client_age = calculate_age_in_years_and_months(client_days_birth)
+        
+        # Calcul de la distribution de l'âge pour tous les clients
+        age_distribution = data_test['DAYS_BIRTH'].apply(calculate_age_in_years_and_months).value_counts().to_dict()
+
+        # Ajouter l'âge du client sélectionné à la réponse
+        age_distribution['client_age'] = client_age
+        return {"age_distribution": age_distribution}
+    else:
+        raise HTTPException(status_code=404, detail="ID client inconnu")
+
+# test local : http://127.0.0.1:8000/distributions/income
+@app.get("/distributions/income", response_model=IncomeDistributionResponseModel)
+def get_income_distribution():
+    income_distribution = data_test[data_test['AMT_INCOME_TOTAL'] < 2000000]['AMT_INCOME_TOTAL'].value_counts().to_dict()
+    return {"income_distribution": income_distribution}
+
 
 # to run the app in local : uvicorn app:app --reload
 # to check the automatic interactive FastAPI documentation : http://127.0.0.1:8000/docs
