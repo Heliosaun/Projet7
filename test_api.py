d971c34e25d003ffad39e025d574a613f664bf69
Heliosaun <119249951+Heliosaun@users.noreply.github.com>
2024-01-13 11:15:50 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -31,3 +31,15 @@ def test_shap_values_client():
     response = client.get("/credit/430080/shap")
     assert response.status_code == 200
     assert "shap_val" in response.json()
+    
+def test_get_age_distribution():
+    # Remplacez 430080 par un ID client valide de votre dataset
+    response = client.get("/credit/430080/age")
+    assert response.status_code == 200
+    assert "age_distribution" in response.json()
+    # Vous pouvez ajouter des assertions supplémentaires pour tester les valeurs spécifiques si nécessaire
+
+def test_get_income_distribution():
+    response = client.get("/distributions/income")
+    assert response.status_code == 200
+    assert "income_distribution" in response.json()
